a42798e2eb648e697d0e1e533e5c2090da54be95
IDRIS <idrisibnshamil@gmail.com>
2025-10-09 18:53:10 +0300

    Update gpk script.js

--- gpk script.js
+++ gpk script.js
@@ -36,6 +36,8 @@ toTop.addEventListener('click', () => {
 const languages = document.querySelectorAll('.language')
 const boll = document.querySelector('.header__columns__second__language--boll')
 const root = document.documentElement
+const red = document.querySelectorAll('.red')
+const blue = document.querySelectorAll('.blue')
 
 languages.forEach((language) => {
     language.addEventListener('click', () => {
@@ -44,7 +46,7 @@ languages.forEach((language) => {
                 boll.style = `
                     left: 48px;
                   `
-            }else {
+            } else {
                 boll.style = `
                      left: 28px;
                   `
@@ -52,11 +54,32 @@ languages.forEach((language) => {
 
             root.style.setProperty('--color-blue', '#e73b3b')
 
+            red.forEach((red) => {
+                red.style = `
+                  opacity: 100;
+                `
+                blue.forEach((blue) => {
+                    blue.style = `
+                      opacity: 0;
+                    `
+                })
+            })
         } else if (language.classList.contains('header__columns__second__language--ru')) {
             boll.style = `
              left: 0px;
         `
             root.style.setProperty('--color-blue', '#3b5de7')
+
+            blue.forEach((blue) => {
+                blue.style = `
+                  opacity: 100;
+                `
+                red.forEach((red) => {
+                    red.style = `
+                      opacity: 0;
+                    `
+                })
+            })
         }
     })
 })
@@ -64,14 +87,38 @@ languages.forEach((language) => {
 // Якорь
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault();
         const target = document.querySelector(this.getAttribute('href'));
         const offset = window.innerHeight / 2;
-        
+
         window.scrollTo({
             top: target.offsetTop - offset,
             behavior: 'smooth'
         });
     });
 });
+
+// Смещение элементов
+
+const elements = document.querySelectorAll('.element')
+
+
+document.addEventListener('mousemove', function (e) {
+
+    const mouseX = e.clientX
+    const mouseY = e.clientY
+
+    elements.forEach(element => {
+        const rect = element.getBoundingClientRect()
+        const centerX = rect.left + rect.width / 2
+        const centerY = rect.top + rect.height / 2
+
+        const moveX = (centerX - mouseX) * 0.05
+        const moveY = (centerY - mouseY) * 0.05
+
+        element.style.transform = `translate(${moveX}px, ${moveY}px)`
+    })
+})
+
+
